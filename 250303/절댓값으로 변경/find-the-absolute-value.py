7c8a99e93fa4102f717b3f57fe3cf7a20dff134d
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2025-03-03 02:01:11 +0900

    Title: 절댓값으로 변경, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250303/절댓값으로 변경/find-the-absolute-value.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def _abs(arr):
+    for i in range(n):
+        if arr[i]<0:
+            arr[i]*=-1
+    return arr
+
+arr=_abs(arr)
+for elem in arr:
+    print(elem, end=' ')
