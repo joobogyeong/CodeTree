d5c8940c14c33e5d42b650462295d6ccaddb65ce
주보경 <powerbodol01@gmail.com>
2025-03-09 17:16:21 +0900

    Title: 코드네임, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250309/코드네임/code-name.py
+++ 250309/코드네임/code-name.py
@@ -1,17 +1,18 @@
 class User:
-    def __init__(self, codename, score):
+    def __init__(self, codename=" ", score=0):
         self.codename = codename
         self.score = score
 
-users = []  # 사용자 객체를 저장할 리스트
-
-# 5명의 데이터를 입력받아 User 객체 생성
+user = []
 for _ in range(5):
     codename, score = input().split()
-    users.append(User(codename, int(score)))
+    user.append(User(codename, int(score)))
 
-# 점수가 가장 낮은 사용자 찾기
-min_user = min(users, key=lambda user: user.score)
+min_score=user[0].score
+min_user_num=0
+for i in range(1,5):
+    if user[i].score<min_score:
+        min+score = user[i].score
+        min_user_num=i
 
-# 결과 출력
-print(f"{min_user.codename} {min_user.score}")
+print(f"{user[min_user_num].codename} {user[min_user_num].score}")
