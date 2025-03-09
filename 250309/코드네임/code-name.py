72f069a385191696a4c3c3cf29db3aa4ca1a72dd
주보경 <powerbodol01@gmail.com>
2025-03-09 17:15:36 +0900

    Title: 코드네임, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/코드네임/code-name.py
+++ 250309/코드네임/code-name.py
@@ -1,17 +1,17 @@
 class User:
-    def __init__(self, codename=" ", score=0):
+    def __init__(self, codename, score):
         self.codename = codename
         self.score = score
 
-user = []
+users = []  # 사용자 객체를 저장할 리스트
+
+# 5명의 데이터를 입력받아 User 객체 생성
 for _ in range(5):
     codename, score = input().split()
-    user.append(User(codename, int(score)))
+    users.append(User(codename, int(score)))
 
-min_score=user[0].score
-min_user_num=0
-for i in range(1,5):
-    if user[i].score<min_score:
-        min_user_num=i
+# 점수가 가장 낮은 사용자 찾기
+min_user = min(users, key=lambda user: user.score)
 
-print(f"{user[min_user_num].codename} {user[min_user_num].score}")
+# 결과 출력
+print(f"{min_user.codename} {min_user.score}")
