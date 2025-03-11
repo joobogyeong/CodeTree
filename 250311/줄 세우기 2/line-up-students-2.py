6c93794dd19bedf8cfc382384f49049611663454
주보경 <powerbodol01@gmail.com>
2025-03-11 23:46:15 +0900

    Title: 줄 세우기 2, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250311/줄 세우기 2/line-up-students-2.py
+++ 250311/줄 세우기 2/line-up-students-2.py
@@ -5,11 +5,11 @@ class Student:
         self.w=w
         self.num = num
 student=[]
-for i in range(n):
+for i in range(1, n+1):
     h, w = list(map(int, input().split()))
     student.append(Student(h,w,i))
 
 student.sort(key = lambda x:(x.h, -x.w))
 
 for s in student:
-    print(s.h, s.w)
+    print(s.h, s.w, s.num)
