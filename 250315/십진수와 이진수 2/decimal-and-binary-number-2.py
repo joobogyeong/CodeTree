414f3cc2b50b9b11738b2a2c1e16da24c236a04a
주보경 <powerbodol01@gmail.com>
2025-03-15 02:18:03 +0900

    Title: 십진수와 이진수 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250315/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,14 @@
+N=list(map(int, input()))
+num=0
+for i in range(len(N)):
+    num=num*2 + N[i]
+num*=17
+digit=[]
+while True:
+    if num<2:
+        digit.append(num)
+        break
+    digit.append(num%2)
+    num//=2
+for elem in digit[::-1]:
+    print(elem, end='')
