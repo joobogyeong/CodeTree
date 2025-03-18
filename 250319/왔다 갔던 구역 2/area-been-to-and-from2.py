ee03b118f1e6eb8dfe7a4b9f321622505e735f21
주보경 <powerbodol01@gmail.com>
2025-03-19 00:08:46 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -8,12 +8,14 @@ current_point=1000
 for i in range(N):
     if arr[i][1]=='R':
         for _ in range(int(arr[i][0])):
-            result_array[current_point]+=1
             current_point+=1
+            result_array[current_point]+=1
+            
     else:
         for _ in range(int(arr[i][0])):
-            result_array[current_point]+=1
             current_point-=1
+            result_array[current_point]+=1
+            
 cnt=0
 for elem in result_array:
     if int(elem)>=2:
