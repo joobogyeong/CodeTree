cff7cb6ee9d37233f58f4b6bd84abd1f2a74cfbf
주보경 <powerbodol01@gmail.com>
2025-03-19 00:09:05 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,23 +1,21 @@
-N=int(input())
-arr=[
-    tuple(input().split())
-    for _ in range(N)
-]
-result_array=[0]*2000
-current_point=1000
-for i in range(N):
-    if arr[i][1]=='R':
-        for _ in range(int(arr[i][0])):
-            current_point+=1
-            result_array[current_point]+=1
-            
-    else:
-        for _ in range(int(arr[i][0])):
-            current_point-=1
-            result_array[current_point]+=1
-            
-cnt=0
-for elem in result_array:
-    if int(elem)>=2:
-        cnt+=1
+N = int(input())
+arr = [tuple(input().split()) for _ in range(N)]
+
+result_array = [0] * 2000  # 충분한 크기의 배열 확보
+current_point = 1000  # 중앙에서 시작
+
+for distance, direction in arr:
+    distance = int(distance)  # 정수 변환
+
+    if direction == 'R':  # 오른쪽 이동
+        for _ in range(distance):
+            current_point += 1  # 먼저 이동
+            result_array[current_point] += 1  # 방문 횟수 증가
+    else:  # 왼쪽 이동
+        for _ in range(distance):
+            current_point -= 1  # 먼저 이동
+            result_array[current_point] += 1  # 방문 횟수 증가
+
+# 2번 이상 방문한 영역 개수 세기
+cnt = sum(1 for visits in result_array if visits >= 2)
 print(cnt)
