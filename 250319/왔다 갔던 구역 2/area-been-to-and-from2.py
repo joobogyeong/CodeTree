50a66220aa4ff396368a41f075c7d0ba03424910
주보경 <powerbodol01@gmail.com>
2025-03-19 00:24:33 +0900

    Title: 왔다 갔던 구역 2, Time: 56ms, Memory: 16MB, Status: Passed - CodeTree

--- 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250319/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,38 +1,46 @@
-N=int(input())
-arr=[
-    tuple(input().split())
-    for _ in range(N)
-]
-result_array=[0]*2000
-current_point=1000
-for i in range(N):
-    if arr[i][1]=='R':
-        for _ in range(int(arr[i][0])):
-            result_array[current_point]+=1
-            current_point+=1
-    else:
-        for _ in range(int(arr[i][0])):
-            result_array[current_point]+=1
-            current_point-=1
-total_length = 0
-in_segment = False
-segment_length = 0  # 현재 구간의 길이
+OFFSET = 1000
+MAX_R = 2000
 
-for i in range(len(result_array)):
-    if result_array[i] >= 2:
-        if not in_segment:  # 새로운 구간이 시작됨
-            in_segment = True
-            segment_length = 1  # 현재 위치 포함
-        else:
-            segment_length += 1  # 연속된 부분이므로 길이 증가
-    else:
-        if in_segment:  # 구간이 끝났다면 길이 합산
-            total_length += segment_length
-            in_segment = False
-            segment_length = 0  # 초기화
+# 변수 선언 및 입력
+n = int(input())
+segments = []
 
-# 마지막 구간이 종료되지 않았을 경우 처리
-if in_segment:
-    total_length += segment_length
+# 현재 위치
+cur = 0
 
-print(total_length)
+for _ in range(n):
+	distance, direction = tuple(input().split())
+	distance = int(distance)
+	
+	if direction == 'L':
+		# 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+		section_left = cur - distance
+		section_right = cur
+		cur -= distance
+	else:
+		# 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+		section_left = cur
+		section_right = cur + distance
+		cur += distance
+	
+	segments.append([section_left, section_right])
+
+	
+checked = [0] * (MAX_R + 1)
+
+for x1, x2 in segments:
+	# OFFSET을 더해줍니다.
+	x1, x2 = x1 + OFFSET, x2 + OFFSET
+	
+	# 구간을 칠해줍니다.
+	# 구간 단위로 진행하는 문제이므로
+	# x2에 등호가 들어가지 않음에 유의합니다.
+	for i in range(x1, x2):
+		checked[i] += 1
+
+# 2번 이상 지나간 영역의 크기를 구합니다.
+cnt = 0
+for elem in checked:
+	if elem >= 2:
+		cnt += 1
+print(cnt)
