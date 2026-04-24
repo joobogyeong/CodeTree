dab52b7788d07b7dfea6c5b1144201ce23468a7f
주보경 <powerbodol01@gmail.com>
2026-04-25 02:28:32 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260425/피보나치 수/fibonacci-number.py
@@ -0,0 +1,13 @@
+N = int(input())
+memo = [-1]*
+
+def fibo(n):
+    if memo[n]!=-1:
+        return memo[n]
+    elif n<=1:
+        memo[n]=1
+    else:
+        memo[n] = fibo(n-1)+fibo(n-2)
+    return memo[n]
+
+print(fibo(N-1))
