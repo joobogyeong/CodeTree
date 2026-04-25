1611ce33b43d6fc598a6dd85cab32cc65e9f90f7
주보경 <powerbodol01@gmail.com>
2026-04-26 02:43:41 +0900

    Title: 비율 구하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260426/비율 구하기/find-proportions.py
@@ -0,0 +1,13 @@
+from sortedcontainers import SortedDict
+N = int(input())
+sd = SortedDict()
+
+for _ in range(N):
+    color = input()
+    if  color not in sd:
+        sd[color]=1
+    else:
+        sd[color]+=1
+
+for color, num in sd.items():
+    print(f"{color} {100//N*num:.4f}")
