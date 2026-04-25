e7ff14fb21c0d867829293df7c8fae568fc884c7
주보경 <powerbodol01@gmail.com>
2026-04-26 01:55:02 +0900

    Title: hashmap 기본, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260426/hashmap 기본/hashmap-basic.py
+++ 260426/hashmap 기본/hashmap-basic.py
@@ -1,24 +1,16 @@
 N = int(input())
 d = {}
-cmd = []
-
-line = [list(input().split()) for _ in range(N)]
-
-
-def add(k, v):
-    d[k]=v
-def remove(k):
-    d.pop(k)
-def find(k):
-    if k in d:
-        print(d[k])
-    else:
-        print("None")
 
 for i in range(N):
-    if line[i][0] == "add":
-        add(line[i][1], line[i][2])
-    elif [i]=="remove":
-        remove(line[i][1])
-    elif cmd[i]=="find":
-        find(line[i][1])
+    cmd = list(input().split())
+    if cmd[0]=='add':
+        d[int(cmd[1])]=int(cmd[2])
+    elif cmd[0]=='remove':
+        d.pop(int(cmd[1]))
+    elif cmd[0]=='find':
+        if int(cmd[1]) in d:
+            print(d[int(cmd[1])])
+        else:
+            print('None')
+
+
