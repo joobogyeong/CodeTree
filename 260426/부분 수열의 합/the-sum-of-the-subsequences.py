b7058d49762735f3c7349ea6096afba5ef9984e4
주보경 <powerbodol01@gmail.com>
2026-04-26 01:15:38 +0900

    Title: 부분 수열의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260426/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,18 @@
+n, m = tuple(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+
+dp = [[False]*(m+1) for _ in range(n+1)]
+
+dp[0][0]=True
+
+for i in range(1, n+1):
+    for j in range(m+1):
+        if j >= arr[i] and dp[i-1][j-arr[i]]:
+            dp[i][j] = True
+        if dp[i-1][j]:
+            dp[i][j] = True
+
+if dp[n][m]:
+    print("Yes")
+else:
+    print("No")
