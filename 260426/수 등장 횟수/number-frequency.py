d04ae37eb39627f2bba3a6fce07effba6fb7bdc0
주보경 <powerbodol01@gmail.com>
2026-04-26 02:18:57 +0900

    Title: 수 등장 횟수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260426/수 등장 횟수/number-frequency.py
+++ 260426/수 등장 횟수/number-frequency.py
@@ -1,14 +1,16 @@
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
+cmd = list(map(int, input().split()))
 d = {}
 
-for _ in range(M):
-    cmd = int(input())
-    d[cmd] = 0
+for i in range(N):
+    if arr[i] not in d:
+        d[arr[i]] = 1
+    else:
+        d[arr[i]]+=1
 
-for i in range(M):
-    for j in range(N):
-        if arr[i]==d[j]:
-            d[j]+=1
-
-print(*d)
+for elem in cmd:
+    if elem not in d:
+        print(0)
+    else:
+        print(d[elem], end=" ")
