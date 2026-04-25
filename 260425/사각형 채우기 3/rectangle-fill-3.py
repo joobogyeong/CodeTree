48a21dfdc7cf42141fd897a8d0ce452431fc710c
주보경 <powerbodol01@gmail.com>
2026-04-25 22:53:37 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260425/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,15 @@
+N = int(input())
+memo = [-1] * (N+1)
+
+def dp(n):
+    if memo[n]!=-1:
+        return memo[n]
+    elif n==1:
+        memo[n]=2
+    elif n==2:
+        memo[n]=7
+    else:
+        memo[n]=2*(dp(n-1))+3*(dp(n-2))+2*(dp(n-3))
+    return memo[n]
+
+print(dp(N)%1000000007)
