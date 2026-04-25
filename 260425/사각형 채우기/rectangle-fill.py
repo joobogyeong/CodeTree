f981a5b0be12c4c5f155c8fe45bc0faa14ec27b2
주보경 <powerbodol01@gmail.com>
2026-04-25 22:32:53 +0900

    Title: 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,15 @@
+N = int(input())
+memo = [-1] * (N+1)
+
+def dp(n):
+    if memo[n]!=-1:
+        return memo[n]
+    elif n==1:
+        memo[n]=1
+    elif n==2:
+        memo[n]=2
+    else:
+        memo[n]=dp(n-1)+dp(n-2)
+    return memo[n]
+
+print(dp(N))
