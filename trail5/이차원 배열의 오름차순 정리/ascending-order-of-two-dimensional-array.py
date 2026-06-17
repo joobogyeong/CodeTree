34059a2c2b60ebf3036c7f6db856f30053f0f3d9
주보경 <powerbodol01@gmail.com>
2026-06-17 19:11:09 +0900

    [Passed] 이차원 배열의 오름차순 정리 (64ms, 17MB)

---
+++ trail5/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.py
@@ -0,0 +1,21 @@
+N = int(input())
+K = int(input())
+
+left = 1
+right = N * N
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+
+    count = 0
+    for i in range(1, N + 1):
+        count += min(N, mid // i)
+
+    if count >= K:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
