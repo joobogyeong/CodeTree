c69742a3d681a5bbda4f21d89fe1265a51afaf96
주보경 <powerbodol01@gmail.com>
2026-06-19 01:50:04 +0900

    [Passed] 최소 스패닝 트리 9 (563ms, 38MB)

---
+++ trail6/최소 스패닝 트리 9/minimum-spanning-tree-9.py
@@ -0,0 +1,38 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b, w = map(int, input().split())
+
+    graph[a].append((w, b))
+    graph[b].append((w, a))
+
+visited = [False] * (n + 1)
+
+pq = []
+heapq.heappush(pq, (0, 1)) 
+
+answer = 0
+count = 0
+
+while pq and count < n:
+    w, node = heapq.heappop(pq)
+
+    if visited[node]:
+        continue
+
+    visited[node] = True
+    answer += w
+    count += 1
+
+    for next_w, next_node in graph[node]:
+        if not visited[next_node]:
+            heapq.heappush(pq, (next_w, next_node))
+
+print(answer)
