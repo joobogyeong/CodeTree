ea508f0ec2310f2bf80f15da8cb0c9cbb65c1f71
주보경 <powerbodol01@gmail.com>
2026-06-22 11:23:35 +0900

    [Passed] 사이클 판별 (157ms, 24MB)

---
+++ trail6/사이클 판별/detect-cycle.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+
+for i in range(1, n + 1):
+    if indegree[i] == 0:
+        q.append(i)
+
+count = 0
+
+while q:
+    cur = q.popleft()
+    count += 1
+
+    for nxt in graph[cur]:
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            q.append(nxt)
+
+if count == n:
+    print("Not Exists")
+else:
+    print("Exists")
